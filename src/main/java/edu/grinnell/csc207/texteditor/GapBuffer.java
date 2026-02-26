c09b906234728a3717541a43747e724f746297ee
Daniel <leedanie2@grinnell.edu>
2026-02-25 23:19:59 -0600

    Co-authored-by: Oscar To <notAnt1@users.noreply.github.com>

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -4,54 +4,94 @@ package edu.grinnell.csc207.texteditor;
  * A gap buffer-based implementation of a text buffer.
  */
 public class GapBuffer implements Buffer {
+    
+    private char[] buffer;
+    // indices of gap
+    private int firstIndex;
+    private int lastIndex;
+    // size of array
+    private int sz = 10;
+
+    /**
+     * Constructor for buffer
+     */
+    public GapBuffer() {
+        buffer = new char[sz];
+        firstIndex = 0;
+        lastIndex = sz - 1;
+    }
+
 
     /**
      * Insert a char to the right of the cursor
      */
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if(firstIndex == lastIndex) {
+            char[] tmpBuff = new char[2*sz];
+            for(int i = 0; i < firstIndex; i++) {
+                tmpBuff[i] = buffer[i];
+            }
+            for(int i = firstIndex; i < sz; i++) {
+                tmpBuff[i + firstIndex] = buffer[i];
+            }
+            buffer = tmpBuff;
+            lastIndex += sz;
+            sz *= 2;
+        }
+        buffer[firstIndex] = ch;
+        firstIndex++;
     }
 
     /**
      * Delete the char to the left of the cursor
      */
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if(firstIndex > 0) {
+            firstIndex--;
+        }
     }
 
     /**
      * @returns the int index of the cursor position
      */
     public int getCursorPosition() {
-        throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
+        return firstIndex;
     }
 
     /**
      * Move the cursor to the left
      */
     public void moveLeft() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        buffer[lastIndex-1] = buffer[firstIndex - 1];
+        firstIndex--;
+        lastIndex--;
     }
 
     /**
      * Move the cursor to the right
      */
     public void moveRight() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+        buffer[firstIndex] = buffer[lastIndex];
+        firstIndex++;
+        lastIndex++;
     }
 
     /**
      * @returns an int size of the buffer
      */
     public int getSize() {
-        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+        return sz;
     }
 
     /**
      * @returns a char at index i
+     * @throws IndexOutOfBoundsException() if user passes in an invalid index ffor buffer
      */
     public char getChar(int i) {
-        throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+        if(i >= sz || i < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        return buffer[i];
     }
 
     /**
@@ -59,6 +99,14 @@ public class GapBuffer implements Buffer {
      * @returns a string 
      */
     public String toString() {
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        String result = new String();
+        result = "";
+        for(int i = 0; i < firstIndex; i++) {
+            result = result + String.valueOf(buffer[i]);
+        }   
+        for(int i = lastIndex; i < sz; i++) {
+            result = result + String.valueOf(buffer[i]);
+        }
+        return result;
     }
 }
