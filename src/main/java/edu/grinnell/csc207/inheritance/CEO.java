ad5f3ce322116f4d0ba0fa3ccbbcb45006bf909a
Daniel <leedanie2@grinnell.edu>
2026-02-25 22:40:38 -0600

    Finished inheritance lab

--- src/main/java/edu/grinnell/csc207/inheritance/CEO.java
+++ src/main/java/edu/grinnell/csc207/inheritance/CEO.java
@@ -3,13 +3,7 @@ package edu.grinnell.csc207.inheritance;
 import java.util.List;
 
 /** A CEO. */
-public class CEO {
-
-    private String name;
-
-    private String id;
-
-    private List<Employee> reportees;
+public class CEO extends Manager {
 
     /**
      * Constructs a new CEO.
@@ -19,29 +13,8 @@ public class CEO {
      * @param reportees the CEO's direct reports.
      */
     public CEO(String name, String id, List<Employee> reportees) {
-        this.name = "(CEO) " + name;
-        this.id = id;
-        this.reportees = reportees;
+        super("(CEO) " + name, id, reportees);
+        this.salary = 80000 * 100;
+        this.noise = "Do More Work More Do More Work More Do More Work";
     }
-
-    /** @return the CEO's name. */
-    public String getName() {
-        return name;
-    }
-
-    /** @return the CEO's ID. */
-    public String getId() {
-        return id;
-    }
-
-    /** @return the CEO's salary. */
-    public int getSalary() {
-        return 80000 * 100;
-    }
-
-    /** @return the CEO's work noise. */
-    public String makeWorkNoise() {
-        return "Do More Work More Do More Work More Do More Work";
-    }
-
 }
--- src/main/java/edu/grinnell/csc207/inheritance/Manager.java
+++ src/main/java/edu/grinnell/csc207/inheritance/Manager.java
@@ -3,11 +3,7 @@ package edu.grinnell.csc207.inheritance;
 import java.util.List;
 
 /** A manager. */
-public class Manager implements Employee {
-
-    private String name;
-
-    private String id;
+public class Manager extends Employee {
 
     private List<Employee> reportees;
 
@@ -19,29 +15,7 @@ public class Manager implements Employee {
      * @param reportees the manager's direct reports.
      */
     public Manager(String name, String id, List<Employee> reportees) {
-        this.name = "(MGR) " + name;
-        this.id = id;
+        super("(MGR) " + name, id, 120000 * 3, "Do Work Do Work Do Work");
         this.reportees = reportees;
     }
-
-    /** @return the manager's name. */
-    public String getName() {
-        return name;
-    }
-
-    /** @return the manager's ID. */
-    public String getId() {
-        return id;
-    }
-
-    /** @return the manager's salary. */
-    public int getSalary() {
-        return 120000 * 3;
-    }
-
-    /** @return the manager's work noise. */
-    public String makeWorkNoise() {
-        return "Do Work Do Work Do Work";
-    }
-
 }
